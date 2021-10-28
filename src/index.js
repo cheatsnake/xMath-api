a95a8637044e8eb90f8e817bfa0bb0e976956d37
Yury <yura.sviderko@live.com>
2021-10-28 14:54:58 +0300

    update API router import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import {router} from './routes/router.js';
+import apiRouter from './routes/api.router.js';
 
 dotenv.config();
 
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 app.use('/', express.static(__dirname + '/../public'));
-app.use('/api', router);
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT} port.`);
